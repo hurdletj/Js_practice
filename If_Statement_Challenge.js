ded26112bbd26cb93050cf273840799464f28737
Frugal Freddy <103449694+hurdletj@users.noreply.github.com>
2022-06-02 20:29:15 -0700

    init

--- If_Statement_Challenge.js
+++ If_Statement_Challenge.js
@@ -22,3 +22,115 @@ Log a message to the console saying:("Yes I think I need an oil change.")
 If not
 Log a message to the console saying:("My car doesn't need an oil change yet.")
 */
+
+// console.log("Welcome to functions!");
+
+// 1. Define your function
+
+// function sayHello() {
+//   console.log("Hello!");
+// }
+
+// 2. Call your function
+
+// sayHello();
+
+// // Methods: functions stored on an object
+// console.log();
+// Array.push();
+// console.log("Hello everybody!");
+
+// Define a function with a parameter
+// function sayHello(firstName) {
+//   console.log(`Hello, ${firstName}`);
+// }
+
+// Call the function with an argument
+// sayHello("Bob");
+
+// function restaurantGreeting(firstName) {
+//   console.log(`Welcome to Red Lobster, ${firstName}`);
+
+// //   console.log(firstName); // This works!
+// }
+
+// restaurantGreeting("Larry");
+
+// console.log(firstName); // This doesn't work! :-(
+
+// prompt("What is your favorite ice cream?"); // returns a string
+
+// Functions can return values.
+
+// function add(numOne, numTwo) {
+//   return numOne + numTwo;
+// }
+
+// const total = add(1, 2);
+
+// console.log(total);
+
+// A function that does NOT return a value is called a void function.
+
+// console.log("Welcome to functions!");
+
+// 1. Define your function
+
+// function sayHello() {
+//   console.log("Hello!");
+// }
+
+// 2. Call your function
+
+// sayHello();
+
+// // Methods: functions stored on an object
+// console.log();
+// Array.push();
+// console.log("Hello everybody!");
+
+// Define a function with a parameter
+// function sayHello(firstName) {
+//   console.log(`Hello, ${firstName}`);
+// }
+
+// Call the function with an argument
+// sayHello("Bob");
+
+// function restaurantGreeting(firstName) {
+//   console.log(`Welcome to Red Lobster, ${firstName}`);
+
+// //   console.log(firstName); // This works!
+// }
+
+// restaurantGreeting("Larry");
+
+// console.log(firstName); // This doesn't work! :-(
+
+// prompt("What is your favorite ice cream?"); // returns a string
+
+// Functions can return values.
+
+// function add(numOne, numTwo) {
+//   return numOne + numTwo;
+// }
+
+// const total = add(1, 2);
+
+// console.log(total);
+
+// A function that does NOT return a value is called a void function.
+
+function generateArrayOfOddNumbers(maxNumber) {
+    // Modulus operator
+    const oddNumbers = [];
+    for (let i = 0; i < maxNumber; i++) {
+      if (i % 2 === 1) {
+        oddNumbers.push(i);
+      }
+    }
+    return oddNumbers;
+  }
+  
+  const myArray = generateArrayOfOddNumbers(1000);
+  console.log(myArray);
