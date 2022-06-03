7123240b090bca89bdc11070c6bf0b57efd7925f
Frugal Freddy <103449694+hurdletj@users.noreply.github.com>
2022-06-02 18:55:38 -0700

    init

---
+++ If_Statement_Challenge.js
@@ -0,0 +1,24 @@
+/*
+Challenge 1
+
+Do You Need More Coffee?
+
+Write a script that:
+Stores the number of cups (that a person has consumed) in a variable
+If the person has had *less than* 3 cups
+Log a message to the console saying: ("Yes I'll take another cup of coffee.")
+If not
+Log a message to the console saying ("I think I'm okay for now.")
+
+
+
+Challenge 2
+
+Does your car need an oil change?
+
+Write a script that: Stores the number of miles(that a car has) in a variable
+If the car has *more than* 500 miles
+Log a message to the console saying:("Yes I think I need an oil change.")
+If not
+Log a message to the console saying:("My car doesn't need an oil change yet.")
+*/
